8d3765d67f7ad700348a18079795c0700e42fce9
Mateusz Gałuszka <galuszka1996@gmail.com>
2021-06-21 18:27:20 +0200

    add console.log

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
-function getSubtraction(a, b) {
-    let result = a - b;
+'use strict';
+
+function titleClickHandler(event){
+  console.log('Link was clicked!');
+  console.log(event);
+
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
   
-    if(result < 0){
-      let multiplier = -1;
-      result = result * multiplier;
-    }
-  
-    return result;
-  }
-  
-  console.log( getSubtraction(5, 7) );
+}
