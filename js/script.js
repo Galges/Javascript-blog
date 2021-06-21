d7a6599e2bad06bea9e1ed16b5b189aff9713e18
Mateusz Gałuszka <galuszka1996@gmail.com>
2021-06-21 19:26:49 +0200

    titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!');
+    event.preventDefault();
+    const clickedElement = this;
+    console.log('Link was clicked!');
   console.log(event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
 
   /* add class 'active' to the clicked link */
-
+clickedElement.classList.add('active');
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.post');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
+  let articleSelection=clickedElement.getAttribute('href');
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  let targetArticle = document.querySelector(articleSelection);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
