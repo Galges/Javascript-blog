10c92791f52a85842a2fb003f94d3730534f4626
Mateusz Gałuszka <galuszka1996@gmail.com>
2021-06-21 21:33:35 +0200

    titlelist

--- js/script.js
+++ js/script.js
@@ -32,10 +32,52 @@ clickedElement.classList.add('active');
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
 
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+let html='';
+  /* remove contents of titleList */
+const titleList = document.querySelector(optTitleListSelector);
+
+function clearMessages() {
+    titleList.innerHTML = '';
+  }
+
+  clearMessages();
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  for(let article of articles){
+
+  const articleId = article.getAttribute('id');
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  html = html + linkHTML;
+}
+
+titleList.innerHTML = html;
+const links = document.querySelectorAll('.titles a');
+console.log(links);
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
   
+  
 }
+    /* get the article id */
+  
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+  
+}
+
+generateTitleLinks();
